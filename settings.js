6a7145fe7ac7b922f00d23cc9a216a2a548bcfe3
XdimsBOT <106574709+XdimsBOT@users.noreply.github.com>
2022-05-31 12:05:21 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281299609670','6281299609670','6281299609670'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281299609670'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281299609670' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'XdimsDev' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'XdimsBOT' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'XdimsDev' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.ig = 'https://instagram.com/xdimsdev'
+global.mygc = 'https://chat.whatsapp.com/G37TM7y5IcRFtczmknfLER'
+global.myweb = 'linkr.bio/xdimsdev'
+global.email = 'xdimsbot@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© XdimsBOT' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'XdimsDev' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
